fa6e15469f9d10b48365a21cafb1fb7562c0e9bc
Atugatran <atugatran@gmail.com>
2022-11-13 01:17:30 +0530

    Add Top-section in page.html

--- js/page.js
+++ js/page.js
@@ -26,27 +26,53 @@ function loadApis() {
     for (let j = -1; j < Movie.length; j++) {
       let ApiKey = Key[j];
       if (ApiKey === urlKey) {
-        let data =  Movie[ApiKey - 1]
-        const { Poster, Name, Key, Release, Genres, IMDB, Country } = data;
+        let data = Movie[ApiKey - 1];
+        const {
+          Title,
+          Desc,
+          Poster,
+          Name,
+          Key,
+          Release,
+          Genres,
+          IMDB,
+          Country,
+        } = data;
         document.title = Name;
-
-        return `
-        <section class="movie-section" key=${Key}>
-        <div class="movie-card">
-          <figure>
-            <img src="${Poster}" alt="image" />
-          </figure>
-          <div class="card-content">
-            <p class="title">${Name}</p>
-            <p class=""></p>
-            <p class="card-text">${Release}</p>
-            <p class="card-text">${Genres}</p>
-            <p class="card-text">${IMDB} / 10</p>
-            <p class="card-text">${Country}</p>
-          </div>
-        </div>
-      </section>
-          `;
+        return ` 
+  <section class="movie-section" key=${Key}>
+  <!-- Top -->
+  <div class="top-section">
+    <h1 class="heading">${Title}</h1>
+    <p class="desc">${Desc}</p>
+  </div>
+  <!-- Card -->
+  <div class="movie-card">
+    <figure>
+      <img src="${Poster}" alt="image" />
+    </figure>
+    <div class="card-content">
+      <p class="title">${Name}</p>
+      <p class="card-text">${Release}</p>
+      <p class="card-text">${Genres}</p>
+      <p class="card-text">${IMDB} / 10</p>
+      <p class="card-text">${Country}</p>
+    </div>
+  </div>
+  <!-- Bottom -->
+  <!-- <div class="Bottom-section">
+    <div class="MovieInfo_cont">
+      <table class="MovieInfo">
+        <h2 class="subheading">Movie Info:</h2>
+        <tr>
+          <th ckass="data" >Movie Name:</th>
+          <td ckass="data" >${Name}</td>
+        </tr>
+      </table>
+    </div>
+  </div>
+  </section> -->
+  `;
       }
     }
   });
